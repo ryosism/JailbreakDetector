e605f09e68e5a036563210c9584b85fc96b1b3d6
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2024-01-13 19:12:14 +0900

    Initial Commit

---
+++ JailbreakDetector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JailbreakDetector
+//
+//  Created by ryosism on 2024/01/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JailbreakDetector/JailbreakDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JailbreakDetectorApp.swift
+//  JailbreakDetector
+//
+//  Created by ryosism on 2024/01/13.
+//
+
+import SwiftUI
+
+@main
+struct JailbreakDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
