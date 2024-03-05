5be95695a73371b828e271d5daab3978aa7b9ef4
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2024-03-06 00:16:41 +0900

    SPM関連ファイルの追加

---
+++ Tests/JailbreakDetectorTests/JailbreakDetectorTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import JailbreakDetector
+
+final class JailbreakDetectorTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
